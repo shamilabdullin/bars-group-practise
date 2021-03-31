7d2b88ed112e23f35f77db1e29e0a7bac6856c9e
Shamil <31711606+shamilabdullin@users.noreply.github.com>
2021-03-31 19:24:37 +0300

    Update SportDto.cs

--- Teams/Data/Dto/SportDto.cs
+++ Teams/Data/Dto/SportDto.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Judges.Data.Models
+namespace Teams.Data.Models
 {
     public class SportDto
     {
